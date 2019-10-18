4058a163c6c9ece0416280bba152094ffa4c72ca
AlejandraMoralesB <a.moralesbarra@gmail.com>
2019-10-17 21:30:08 -0300

    Updates

---
+++ precache-manifest.d95cbfad041886f64f026d0b9c9ccfa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "503c7a881b75611481e7bec2e94616a0",
+    "url": "/-Desafio-R-plicaDePinterest/index.html"
+  },
+  {
+    "revision": "94dc54f96e44a22e779b",
+    "url": "/-Desafio-R-plicaDePinterest/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "0424b739bf5e0e7bd3a8",
+    "url": "/-Desafio-R-plicaDePinterest/static/css/main.9d6ecae6.chunk.css"
+  },
+  {
+    "revision": "94dc54f96e44a22e779b",
+    "url": "/-Desafio-R-plicaDePinterest/static/js/2.58b3d6c9.chunk.js"
+  },
+  {
+    "revision": "0424b739bf5e0e7bd3a8",
+    "url": "/-Desafio-R-plicaDePinterest/static/js/main.443fad9e.chunk.js"
+  },
+  {
+    "revision": "eb9396bbccb1f88db639",
+    "url": "/-Desafio-R-plicaDePinterest/static/js/runtime-main.5eb35ed8.js"
+  }
+]);
